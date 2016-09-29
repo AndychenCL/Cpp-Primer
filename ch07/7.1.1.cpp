b77146c4c0829308b793c36b8f92aa3839a457a8
AndychenCL <andychen.gmcl@gmail.com>
2016-09-29 18:02:17 +0800

    Use combine function

--- ch07/7.1.1.cpp
+++ ch07/7.1.1.cpp
@@ -7,12 +7,16 @@ using namespace std;
 
 int main(){
 	Sales_data total;
+	
+	cout<< "Please enter:\n\nBookNo\tUnits\tRevenue\n\n" <<endl;
 	if(cin>>total.bookNo >>total.units_sold >>total.revenue){
 		Sales_data trans;
 		while(cin>>trans.bookNo >>trans.units_sold >>trans.revenue){
 			if(total.bookNo == trans.bookNo){
-				total.units_sold += trans.units_sold;
-				total.revenue += trans.revenue;
+				//total.units_sold += trans.units_sold;
+				//total.revenue += trans.revenue;
+				// use combine function to procces this 
+				total.combine(trans);
 			}else{
 				cout << total.bookNo << " "<< total.units_sold <<" "<<total.revenue <<endl;
 				total = trans;
