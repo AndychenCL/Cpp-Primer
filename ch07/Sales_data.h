70fedd1229ff82666467f782c1fe3ae8f77450ff
AndychenCL <andychen.gmcl@gmail.com>
2016-10-03 17:11:30 +0800

    move istream into Sales_data

--- ch07/Sales_data.h
+++ ch07/Sales_data.h
@@ -6,6 +6,11 @@
 #ifndef SALES_DATA_H
 #define SALES_DATA_H
 #include <string>
+#include <iostream>
+
+struct Sales_data;
+std::istream &read(std::istream&, Sales_data&);
+
 struct Sales_data{
 	
 	//constructor
@@ -13,7 +18,9 @@ struct Sales_data{
 	Sales_data(const std::string &s): bookNo(s){}
 	Sales_data(const std::string &s, unsigned n, double p):
 				bookNo(s), units_sold(n), revenue(p*n){}
-	Sales_data(std::istream &);
+	Sales_data(std::istream &is){
+		read(is,*this);
+	}
 	
 	
 	//old data.
@@ -28,9 +35,22 @@ struct Sales_data{
 };
 
 //nonmember function
-Sales_data add(const Sales_data&, const Sales_data&);
-std::ostream &print(std::ostream&, const Sales_data&);
-std::istream &read(std::istream&, Sales_data&);
+Sales_data add(const Sales_data &lhs, const Sales_data &rhs){
+	Sales_data sum = lhs;
+	sum.combine(rhs);
+	return sum;
+}
+
+std::ostream &print(std::ostream &os, const Sales_data &item){
+	os<< item.isbn() <<" " << item.units_sold<< " "<< item.revenue;
+}
+
+std::istream &read(std::istream &is, Sales_data &item){
+	double price =0;
+	is >> item.bookNo >> item.units_sold>> price;
+	item.revenue = price * item.units_sold;
+	return is;
+}
 
 
 
@@ -42,8 +62,8 @@ Sales_data& Sales_data::combine(const Sales_data &rhs){
 }
 
 //constructor
-Sales_data::Sales_data(std::istream &is){
-	read(is,*this);
-}
+//Sales_data::Sales_data(std::istream &is){
+//	read(is,*this);
+//}
 
 #endif
