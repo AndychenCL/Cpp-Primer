72d92a364f6c610fcbe957916ab923a63a76a070
AndychenCL <andychen.gmcl@gmail.com>
2016-10-03 17:12:15 +0800

    use class to ddefine Sales_data

--- ch07/7.11.cpp
+++ ch07/7.11.cpp
@@ -5,16 +5,27 @@
 
 #include <iostream>
 #include <string>
-#include "Sales_data.h"
+#include "Sales_data_c.h"
 
 using namespace std;
 
 
 int main(){
 	Sales_data total;
+	print(cout, total);
+	printf("\n");
 	
+	Sales_data t2("My-time");
+	print(cout, t2);
+	printf("\n");
 	
+	Sales_data t3("My-time", 34, 86.34);
+	print(cout,t3);
+	printf("\n");
 	
+	Sales_data t4(cin);
+	print(cout,t4);
+	printf("\n");
 	
 	return 0;
 }
